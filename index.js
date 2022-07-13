da3421bd00cede9c842f600a503ec30894e11b0d
Bailey Cage <bailey.cage@gmail.com>
2022-07-13 19:47:14 -0400

    commit tree ec47bd68b08d9f7c946aa0325c28ee0d9f800f99

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+    return gifts;
+}
+
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; ++i) {
+        messages[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    }
+    return messages;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        --number;
+    }
+    return;
+}
+
+// wrapGifts(gifts);
+// console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+// countDown(10);
